53169774c8babb49b65f0db2529eb2545df92ebe
桑叁年 <15459398+sang-san-nian@user.noreply.gitee.com>
2025-05-16 16:45:57 +0800

    配置.gitignore

--- Objects/vl5300_demo.d
+++
@@ -1,7 +0,0 @@
-.\objects\vl5300_demo.o: Jia\My\VL5300\My\VL5300_Demo.c
-.\objects\vl5300_demo.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\vl5300_demo.o: .\Jia\My\Delay\Delay_API_z.h
-.\objects\vl5300_demo.o: .\Jia\My\OLED\OLED_API.h
-.\objects\vl5300_demo.o: .\Jia\My\OLED\OLED_Func.h
-.\objects\vl5300_demo.o: .\Jia\My\OLED\OLED_Demo.h
-.\objects\vl5300_demo.o: Jia\My\VL5300\My\VL5300_API.h
